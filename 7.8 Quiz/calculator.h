5728f6c8326dc8c0053848f58eb9af4b41238811
Po-Shan Shih <tommyannypeter@gapp.nthu.edu.tw>
2018-08-24 23:42:14 +0800

    optimize the array in 7.8 Quiz

--- 7.8 Quiz/calculator.h
+++ 7.8 Quiz/calculator.h
@@ -15,19 +15,12 @@ int subtract(int value1, int value2);
 int multiply(int value1, int value2);
 int divide(int value1, int value2);
 
-// static const std::array<arithmeticStruct, 4> g_arithmeticArray {
-//     {'+', add},
-//     {'-', substract},
-//     {'*', multiply},
-//     {'/', divide}
-// };
-
-static const arithmeticStruct g_arithmeticArray[] {
-	{ '+', add },
-	{ '-', subtract },
-	{ '*', multiply },
-	{ '/', divide }
-};
+static const std::array<arithmeticStruct, 4> g_arithmeticArray {{
+    {'+', add},
+    {'-', subtract},
+    {'*', multiply},
+    {'/', divide}
+}};
 
 arithmeticFcn getArithmetricFunction(char oper);
 
